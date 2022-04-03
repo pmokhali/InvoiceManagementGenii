c744acc75b3e15b03dea27694aa1cfc8bc20de2b
Phethiso Mokhali <PhethisoM@lewisgroup.co.za>
2022-04-03 12:03:45 +0200

    PS - Added styles and masterpage

---
+++ InvoiceManagement/styles/js/pageControl.js
@@ -0,0 +1,18 @@
+﻿function populateItemPrice() {
+    // find the dropdown
+    var ddl = document.getElementById("productList");
+    // find the selected option
+    var selectedOption = ddl.selectedIndex;
+    // find the attribute value
+    var mailValue = selectedOption.getAttribute("Value");
+    // find the textbox
+    var textBox = document.getElementById("txtPrice");
+
+    // set the textbox value
+    if (mailValue == "apple") {
+        textBox.value = "1.2";
+    }
+    else {
+        textBox.value = 00;
+    }
+}
